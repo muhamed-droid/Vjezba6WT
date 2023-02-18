f6912b4e9c98e1c1c8ae498626166760138b1673
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-18 11:38:15 +0100

    Zadatak1 i 2 done

--- zadatak1.js
+++ zadatak1.js
@@ -1,13 +1,30 @@
 var http = require('http');
 var fs = require('fs');
-//http.createServer(function (req, res) {
-  //  res.writeHead(200, {'Content-Type': 'text/html'});
-  //  res.write('<h1>Putanja zahtjeva: '+req.url+'</h1>');
-  //  res.end('<b>Metoda:'+req.method+'</b>')
-//}).listen(8080);
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  const queryObject = url.parse(req.url, true).query;
+  const q = queryObject.q;
+
+  if (q) {
+    const filteredLines = imenik.filter(line => line.toLowerCase().includes(q.toLowerCase()));
+    res.write(`<h1>Filtered results for "${q}":</h1>`);
+    filteredLines.forEach(line => {
+      res.write(`<p>${line}</p>`);
+    });
+  } else {
+    res.write('<h1>Putanja zahtjeva: '+req.url+'</h1>');
+    res.write('<b>Metoda:'+req.method+'</b>');
+  }
+  res.end();
+}).listen(8080);
+
+const imenik = fs.readFileSync('imenik.txt', 'utf8').split('\n');
 
 
-fs.readFile('imenik.txt', 'utf8', (err, data) => {
+ fs.readFile('imenik.txt', 'utf8', (err, data) => {
     if (err) throw err;
     const parsedData = {
         imenik: data.split('\n')
@@ -16,4 +33,4 @@ fs.readFile('imenik.txt', 'utf8', (err, data) => {
         if (err) throw err;
         console.log('File written successfully!');
       });
-});
+}); 
